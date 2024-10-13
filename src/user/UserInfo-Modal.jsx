ae4468d6b8eb5f157fc471515963c024df159f66
Yuchan Kim <yuchankim0906@gmail.com>
2024-10-13 13:47:45 +0900

    kyc

--- src/user/UserInfo-Modal.jsx
+++ src/user/UserInfo-Modal.jsx
@@ -1,19 +1,13 @@
-//import 라이브러리
 import React from 'react';
-// import React, {useState} from 'react';	화면 상태관리
-// import { Link } from 'react-router-dom';	링크걸기
-// import { useSearchParams} from 'react-router-dom';	파라미터값사용하는 라우터
-
-//import css
 import '../css/user.css';
 
 const Modal = ({ isOpen, onClose, children }) => {
-    if (!isOpen) return null; // 모달이 열리지 않으면 아무것도 렌더링하지 않음
+    if (!isOpen) return null;
 
     return (
-        <div className="DA-modal-overlay">
-            <div className="DA-modal-content">
-                <button onClick={onClose}>&times;</button>
+        <div className="DA-modal-container">
+            <div className="DA-modal-content apple-modal-content">
+                <button onClick={onClose} className="apple-close-btn">&times;</button>
                 {children}
             </div>
         </div>
